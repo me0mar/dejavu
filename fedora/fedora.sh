5fd48b81afac64b77c5805b596366a1006ea508a
Omar <119429914+me0mar@users.noreply.github.com>
2024-02-04 12:15:03 +0100

    Update fedora.sh

--- fedora/fedora.sh
+++ fedora/fedora.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #!/bin/sh
 
-sudo dnf install firefox clang docker docker-compose keepassxc mpv lxappearance ncmpcpp mpc bat lsd htop jetbrains-mono-fonts dunst torbrowser-launcher dejavu-sans-fonts google-noto-kufi-arabic-fonts google-noto-naskh-arabic-fonts pcmanfm xorg-x11-drv-amdgpu keepassxc docker docker-compose nitrogen neovim libxcb xinit fira-code-fonts pavucontrol xorg-x11-server-Xorg libX11-devel libXft-devel libXinerama-devel 
+sudo dnf install firefox clang docker docker-compose keepassxc mpv lxappearance ncmpcpp mpc bat lsd htop jetbrains-mono-fonts dunst torbrowser-launcher dejavu-sans-fonts google-noto-kufi-arabic-fonts google-noto-naskh-arabic-fonts pcmanfm xorg-x11-drv-amdgpu keepassxc docker docker-compose nitrogen neovim libxcb xinit fira-code-fonts pavucontrol xorg-x11-server-Xorg libX11-devel libXft-devel libXinerama-devel rsync unrar p7zip p7zip-plugins zip unzip 
 
