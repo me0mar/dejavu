abfff9963b60eaffaccdd19821b2784aad32103b
Omar <119429914+me0mar@users.noreply.github.com>
2024-02-19 09:20:14 +0100

    Update dejavu.sh

--- dejavu.sh
+++ dejavu.sh
@@ -1,1 +1,1 @@
-sudo xbps-install base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel xorg xinit git
+sudo xbps-install base-devel libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel xorg xinit 
